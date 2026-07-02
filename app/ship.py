ea03a4eaad05fd3db96ac80cf7e840a690adc310
Murod Akhmedov <python_webdev@outlook.com>
2026-07-02 20:35:35 +0100

    chore (app): add ship file

---
+++ app/ship.py
@@ -0,0 +1,27 @@
+'''Ship module for Alien Invasion game.'''
+
+from pathlib import Path
+
+import pygame
+
+IMAGES_DIR = Path(__file__).resolve().parent / "images"
+
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load(str(IMAGES_DIR / "ship.bmp"))
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
