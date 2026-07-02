16155707d1e31ed1a87a6744b0801d42f01e6bbd
Murod Akhmedov <python_webdev@outlook.com>
2026-07-02 20:42:43 +0100

    refactor(app): update __init__ module

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from .alien_invasion import AlienInvasion
 from .settings import Settings
+from .ship import Ship
 
-__all__ = ["AlienInvasion", "Settings"]
+__all__ = ["AlienInvasion", "Settings", "Ship"]
