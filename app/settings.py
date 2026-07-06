b569b3c4667cfebad5e832bc18cc067d4c5112f1
Murod Akhmedov <python_webdev@outlook.com>
2026-07-06 05:07:20 +0100

    feat(settings): add bullet_speed, bullet_width, bullet_height, bullet_color and bullet_allowed settings

--- app/settings.py
+++ app/settings.py
@@ -15,4 +15,10 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # Ship settings
-        self.ship_speed = 3
+        self.ship_speed = 2
+        # Bullet settings
+        self.bullet_speed = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
